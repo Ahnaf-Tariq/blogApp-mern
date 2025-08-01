aaf4e12b2396c6ed67781d8de912e1cf85f837d1
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-08-01 15:09:46 -0700

    image uploaded feature using cloudinary

--- backend/controllers/blogController.js
+++ backend/controllers/blogController.js
@@ -1,7 +1,30 @@
+import cloudinary from "../middleware/cloudinary.js";
+import blogModel from "../models/BlogModel.js";
+// import { v2 as cloudinary } from "cloudinary";
+
 const addBlog = async (req, res) => {
   try {
+    const { title, description, category } = req.body;
+    const image = req.file;
+
+    if (!image) return res.status(400).json({ msg: "No image provided" });
+
+    const result = await cloudinary.uploader.upload(image.path, {
+      resource_type: "image",
+    });
+
+    const blog = await blogModel.create({
+      title,
+      description,
+      category,
+      image: result.secure_url
+    });
+
+    res.json({ success: true, blog });
+
   } catch (error) {
     console.log(error);
+    res.json({success:false, error})
   }
 };
 
